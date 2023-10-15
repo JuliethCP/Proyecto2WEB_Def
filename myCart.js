a61be0bf3bbc6b88ea8c551b85542dd1f649abdd
Julieth Campos <juliethcp22@gmail.com>
2023-10-15 11:29:15 -0600

    se muestran productos en el carrito

--- myCart.js
+++ myCart.js
@@ -4,44 +4,42 @@ let carrito = JSON.parse(sessionStorage.getItem('carrito')) || [];
 // Obtener el elemento UL para mostrar los productos en el carrito
 const carritoLista = document.getElementById('carrito-lista');
 
+
 // Función para buscar un producto por ID en el inventario
-async function buscarProductoPorId(id) {
+async function buscarProductoPorId(idObj) {
     try {
         const response = await fetch('inventario.json'); // Cargar el archivo inventario.json
         const inventario = await response.json();
+        
+        // Accede al valor 'id' dentro del objeto y conviértelo a número
+        const numeroId = parseInt(idObj.id, 10);
 
-        // Convertir el ID a número
-        const numeroId = parseInt(id, 10);
-
-        // Comprobar si el númeroId es un número válido
+        // Comprueba si el númeroId es un número válido
         if (!isNaN(numeroId)) {
             // Buscar el producto por número de ID
             const producto = inventario.find(producto => producto.id === numeroId);
             return producto;
         } else {
-            console.error('El ID del producto no es un número válido.');
-            return null; // Retorna null si el ID no es válido
+            console.error('El ID del producto no es un número válido:', idObj);
         }
     } catch (error) {
         console.error('Error al cargar el inventario:', error);
-        return null; // Retorna null en caso de error
     }
 }
 
-// Crear un array de promesas para buscar productos por ID
-const promesas = carrito.map(async productoId => buscarProductoPorId(productoId));
+// Iterar sobre los productos en el carrito y mostrarlos
+carrito.forEach(async function(productoIdObj) {
+    // Convierte el ID almacenado como objeto a número antes de buscar el producto
+    const producto = await buscarProductoPorId(productoIdObj);
 
-// Esperar a que todas las promesas se completen
-Promise.all(promesas)
-    .then(resultados => {
-        resultados.forEach(producto => {
-            if (producto) {
-                const li = document.createElement('li');
-                li.innerHTML = `
-                    <img src="${producto.imagenes[0]}" alt="${producto.nombre}" width="50">
-                    <span>${producto.nombre} - Precio: $${producto.precio}</span>
-                `;
-                carritoLista.appendChild(li);
-            }
-        });
-    });
+    if (producto) {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <img src="${producto.imagenes[0]}" alt="${producto.nombre}" width="50">
+            <span>${producto.nombre} - Precio: $${producto.precio}</span>
+        `;
+        carritoLista.appendChild(li);
+    }
+});
+
+
