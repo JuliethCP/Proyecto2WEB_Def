d2811387ed047dc541fc14d0453a5e67c7d2bd4b
nicolejara09 <84295051+nicolejara09@users.noreply.github.com>
2023-10-15 17:08:28 -0600

    preciosura

--- scriptCart.js
+++ scriptCart.js
@@ -1,16 +1,10 @@
-// Event listener para agregar productos al carrito
 document.querySelectorAll('.add-cart').forEach(button => {
     button.addEventListener('click', agregarAlCarrito);
 });
 
-// Event listener para limpiar el carrito al recargar la página
-//window.addEventListener('beforeunload', limpiarCarrito);
-
 // Función para agregar productos al carrito
 function agregarAlCarrito(event) {
-    console.log('Agregando producto al carrito');
     const productId = event.target.getAttribute('data-id');
-    console.log('ID del producto: ' + productId);
     const product = {
         id: productId,
         // Otros datos del producto (nombre, precio, etc.)
@@ -25,18 +19,11 @@ function agregarAlCarrito(event) {
     // Actualizar el carrito en el sessionStorage
     sessionStorage.setItem('carrito', JSON.stringify(carrito));
 
-    // Mostrar el mensaje de confirmación usando un alert
-    alert('Producto agregado al carrito correctamente!');
-
-    // Ocultar el alert después de 2 segundos (2000 milisegundos)
-    setTimeout(function() {
-        // Cerrar el alert (no hay una forma directa de cerrar un alert, por lo que no se puede eliminar, solo esperar el tiempo deseado)
-    }, 2000);
+    // Mostrar mensaje de éxito con SweetAlert2
+    Swal.fire({
+        icon: 'success',
+        title: 'Producto agregado al carrito correctamente!',
+        showConfirmButton: false,
+        timer: 2000 // Cerrar automáticamente después de 2 segundos
+    });
 }
-
-
-// Función para limpiar el carrito
-//function limpiarCarrito() {
-    // Remover el carrito del sessionStorage
-  //  sessionStorage.removeItem('carrito');
-//}
