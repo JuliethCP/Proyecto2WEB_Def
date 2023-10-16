97de95c94fe35355235894a067621eae8222b60d
nicolejara09 <84295051+nicolejara09@users.noreply.github.com>
2023-10-15 18:58:53 -0600

    selogrooooooo

--- scriptCart.js
+++ scriptCart.js
@@ -2,7 +2,6 @@ document.querySelectorAll('.add-cart').forEach(button => {
     button.addEventListener('click', agregarAlCarrito);
 });
 
-// Función para agregar productos al carrito
 function agregarAlCarrito(event) {
     const productId = event.target.getAttribute('data-id');
     const product = {
@@ -10,20 +9,36 @@ function agregarAlCarrito(event) {
         // Otros datos del producto (nombre, precio, etc.)
     };
 
-    // Obtener el carrito de compras actual desde el sessionStorage (si existe)
-    let carrito = JSON.parse(sessionStorage.getItem('carrito')) || [];
+    // Supongamos que hay una función llamada puedeAgregarAlCarrito(productId)
+    // que verifica si el producto puede ser agregado al carrito o no.
+    // Esta función debería devolver true si el producto puede ser agregado y false en caso contrario.
+    const puedeAgregar = puedeAgregarAlCarrito(productId);
 
-    // Agregar el producto al carrito
-    carrito.push(product);
+    if (puedeAgregar) {
+        let carrito = JSON.parse(sessionStorage.getItem('carrito')) || [];
+        carrito.push(product);
+        sessionStorage.setItem('carrito', JSON.stringify(carrito));
 
-    // Actualizar el carrito en el sessionStorage
-    sessionStorage.setItem('carrito', JSON.stringify(carrito));
+        Swal.fire({
+            icon: 'success',
+            title: 'Product added to cart successfully!',
+            showConfirmButton: false,
+            timer: 2000
+        });
+    } else {
+        // Si no se puede agregar, mostrar un mensaje de error
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Could not add product to cart. Please try again later.',
+        });
+    }
+}
 
-    // Mostrar mensaje de éxito con SweetAlert2
-    Swal.fire({
-        icon: 'success',
-        title: 'Producto agregado al carrito correctamente!',
-        showConfirmButton: false,
-        timer: 2000 // Cerrar automáticamente después de 2 segundos
-    });
+function puedeAgregarAlCarrito(productId) {
+    // Lógica para verificar si el producto puede ser agregado al carrito
+    // Devuelve true si se puede agregar, false si no se puede agregar
+    // Puedes implementar tu propia lógica aquí
+    // Por ejemplo, verificar si el producto está en stock, etc.
+    return true; // Devuelve true por defecto en este ejemplo
 }
